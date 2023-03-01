561cfe65f6a2f696a4aaba88d0bab5fb14c4ce28
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-01 00:10:27 -0800

    add and remove GIFs work

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 console.log("Let's get this party started!");
 
 const searchButton = $('#search-giphy');
+const clearButton = $('#clear-all');
 
 async function getAGif() {
-  const searchTerm = ('#search-term').val || 'funny';
+  console.log($('#search-term').val());
+  const searchTerm = $('#search-term').val() || 'funny';
   let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
 
-  // addGifToGallery()
-
-  let image = response.data.data[0].images.original.url;
+  const rIdx = randomIndex();
+  console.log('rIdx', rIdx);
+  let image = response.data.data[rIdx].images.original.url;
   console.log('searchTerm', searchTerm, 'image:', image);
 
-  addGifToGallery(image);
+  addGifToGallery(image, searchTerm);
 }
 
-async function addGifToGallery(gif) {
+async function addGifToGallery(gif, searchTerm) {
   const gallery = $('#gif-gallery');
-  const newGif = $('<img>', {
-    src: gif
+  // const newGif = $('<img>', {
+  //   src: gif,
+  //   class: 'gif'
+  // });
+  const subtitle = $(`<p>${searchTerm}</p>`);
+
+  const gifItemDiv = $('<div>', {
+    class: 'gif-item',
+    style: `background: url(${gif});
+            background-size: cover;
+            background-position: center;`
+
   })
 
-  gallery.append(newGif);
+  // newGif.appendTo(gifItemDiv);
+  subtitle.appendTo(gifItemDiv);
+  gifItemDiv.appendTo(gallery);
+
+}
+
+function clearAll(evt) {
+  $('#gif-gallery').empty();
+}
+
+function randomIndex() {
+  return Math.floor(Math.random() * 50);
 }
 
 searchButton.on('click', getAGif);
+clearButton.on('click', clearAll);
 
 console.log(searchButton);
