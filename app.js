709bc5543b0da87fb06ccf134b71e675afc3a9e5
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-02-28 21:33:17 -0800

    lab time

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("Let's get this party started!");
+
+const searchButton = $('#search-giphy');
+
+async function getAGif() {
+  const searchTerm = ('#search-term').val || 'funny';
+  let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+
+  // addGifToGallery()
+
+  let image = response.data.data[0].images.original.url;
+  console.log('searchTerm', searchTerm, 'image:', image);
+
+  addGifToGallery(image);
+}
+
+async function addGifToGallery(gif) {
+  const gallery = $('#gif-gallery');
+  const newGif = $('<img>', {
+    src: gif
+  })
+
+  gallery.append(newGif);
+}
+
+searchButton.on('click', getAGif);
+
+console.log(searchButton);
